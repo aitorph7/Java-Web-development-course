ea59e64c6f116e8e2e2b664a84f8870b94281051
Aitor Pérez <aitorph7@hotmail.com>
2024-02-21 13:33:46 +0100

    parte final.01 de la 2ª clase de Angular 19.Feb

--- 008.angular/angular-002-directivas/src/app/product-list/product-list.component.ts
+++ 008.angular/angular-002-directivas/src/app/product-list/product-list.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import { Product } from '../models/product.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],//necesito incluir 'NgFor' para declarar el bucle For de la v16
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
 
   names: string[] = ['persona1', 'persona2', 'persona3', 'persona4'];
+
+  // datos demo que se reemplazarán con datos que vengan del backend.
+  products: Product[] = [{id: 1, title: 'producto 1', price: 40.50, available: true},
+  {id: 2, title: 'producto 2', price: 30.50, available: false},
+  {id: 3, title: 'producto 3', price: 35.50, available: true}
+];
+emails: string[] = [];
 
 }
