8cc95f3af4325cabf6e4b6c0229f7de0e154c777
Aitor Pérez <aitorph7@hotmail.com>
2024-03-07 18:46:54 +0100

    comienzo 8ª clase con Angular jue 7Mar24

---
+++ 009.fullstack/fullstack-001/backend/src/test/java/com/certidevs/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certidevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
