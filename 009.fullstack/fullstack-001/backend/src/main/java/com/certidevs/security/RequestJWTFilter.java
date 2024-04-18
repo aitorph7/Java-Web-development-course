9b2004089ba8f5aa25e3dc79ba2ef97947f0e7e9
Aitor Pérez <aitorph7@hotmail.com>
2024-04-18 14:55:57 +0200

    17ª clase FullStack (17 Abr - Final): refactor 'RequestJWTFilter.java' + crear método 'validateTokenAndExtractUser()'

--- 009.fullstack/fullstack-001/backend/src/main/java/com/certidevs/security/RequestJWTFilter.java
+++ 009.fullstack/fullstack-001/backend/src/main/java/com/certidevs/security/RequestJWTFilter.java
@@ -12,6 +12,7 @@ package com.certidevs.security;
 
 import com.certidevs.model.User;
 import com.certidevs.repository.UserRepository;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
@@ -42,10 +43,8 @@ public class RequestJWTFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        log.info("Hola, ejecutando filtro para JWT");
 
         // 1. Extraer de la cabecera Authorization de la request.
-
         String bearerToken = request.getHeader("Authorization");
         if (!StringUtils.hasLength(bearerToken) || !bearerToken.startsWith("Bearer")){
             filterChain.doFilter(request, response);
@@ -55,40 +54,37 @@ public class RequestJWTFilter extends OncePerRequestFilter {
 
         // 2. Verificar el token JWT:
         // TODO almacenar clave en variable de entorno
-        // Long userId = verifyTokenAndExtractUserId(token);
-
-        byte[] key = Base64.getDecoder().decode("PwUAIxpZLYjsyjzi62bY4or99ZLFISl7y47RWBmm+bs=");
-
-        String userId = Jwts.parser()
-                .verifyWith(Keys.hmacShaKeyFor(key))
-                .build()
-                .parseSignedClaims(token)
-                .getPayload()
-                .getSubject();
-
-        log.info("Id de usuario {}", userId);
-
-        // 3. Obtener el usuario de BD cuyo Id he extraído del token JWT:
-        Optional<User> userOptional = this.userRepository.findById(Long.valueOf(userId)); // el String devuelto me lo ha parseado a Long
+        Optional<User> userOptional = validateTokenAndExtractUser(token);
         if (userOptional.isEmpty()) {
-            // no hay usuario (sería un caso raro) devuelvo un error 'Unauthorized 401'
-            log.warn("Usuario con Id {} no encontrado", userId);
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
 
-        // 4. Crear objeto 'Authentication' con datos del usuario y guardarlo en el contexto de seguridad de Spring Security:
+        // 3. Cargar el usuario en el contexto de seguridad de Spring Security:
         // (OBLIGATORIO: TENER EL STARTER 'Spring Security' en el pom.xml)
         User user = userOptional.get();
-        log.info("Rol del usuario {}", user.getRole().toString());
         SimpleGrantedAuthority role = new SimpleGrantedAuthority(user.getRole().toString());
         Authentication auth = new UsernamePasswordAuthenticationToken(user, null, List.of(role));
         // guardar el usuario en una memoria de Spring:
         SecurityContextHolder.getContext().setAuthentication(auth); // permite que en otras partes de la aplicación puedas...
         // ... obtener la autenticación así: SecurityContextHolder.getContext().getAuthentication(auth);
-
-
         // dejar pasar a la petición para que continúe
         filterChain.doFilter(request, response);
+    }
+
+    private Optional<User> validateTokenAndExtractUser(String token) {
+        byte[] key = Base64.getDecoder().decode("PwUAIxpZLYjsyjzi62bY4or99ZLFISl7y47RWBmm+bs=");
+        try {
+            String userId = Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(key))
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload()
+                    .getSubject();
+            return this.userRepository.findById(Long.valueOf(userId));
+        } catch (JwtException e) {
+            log.error("JWT token validation Error");
+            return Optional.empty();
+        }
     }
 }
